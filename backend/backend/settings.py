206b8cefb7c5cf546b185473acb779aa82e03235
adithyaaa23 <adithyacoc23@gmail.com>
2024-09-18 08:54:13 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,4 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000",]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000",
+                       "https://newcloudfrontend.vercel.app",]
